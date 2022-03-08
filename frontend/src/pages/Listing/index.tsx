310b05800b467bf7ff493652b45a3f21d25837e8
Lucas Ladeira <lucas-ladeira@hotmail.com>
2022-03-08 12:06:19 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,32 @@
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 
-
-function Listing(){
+function Listing() {
     return (
-        <Pagination></Pagination>
+        <>
+            <Pagination></Pagination>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard></MovieCard> 
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard></MovieCard> 
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard></MovieCard> 
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard></MovieCard> 
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard></MovieCard> 
+                    </div>
+
+                </div>
+            </div>                     
+        </>
     );
 }
 
